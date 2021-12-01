512fc70f7fa9cf32ec889662d80dc75a782618a9
DeniseYT <denise.ytlin@gmail.com>
2021-12-01 22:20:07 +0000

    initial commit

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import (Flask, render_template)
+
+app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def homepage():
+    """View homepage"""
+
+    return render_template('homepage.html')
+
+if __name__== "__main__":
+    app.run(host="0.0.0.0", debug=True)
